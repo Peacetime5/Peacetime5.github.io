829ce7377878a7a23b2e3a11a067058251eaccf1
Peacetime5 <43578894+Peacetime5@users.noreply.github.com>
2019-06-11 13:34:34 +0200

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,68 @@
+<?php
+   
+
+    session_cache_limiter( 'nocache' );
+    header( 'Expires: ' . gmdate( 'r', 0 ) );
+    header( 'Content-type: application/json' );
+
+
+    $to             = 'javiercordobesp@gmail.com'; //put your email here
+    $email_template = 'simple.html';  // will find it on email-templates/ directory
+
+    $subject    = "SUBJECT";
+    $email      = strip_tags($_POST['email']);
+    $name       = strip_tags($_POST['name']);
+    $message    = nl2br( htmlspecialchars($_POST['message'], ENT_QUOTES) );
+    $result     = array();
+
+
+    if(empty($name)){
+
+        $result = array( 'response' => 'error', 'empty'=>'name', 'message'=>'<strong>Error!</strong> El nombre está vacío.' );
+        echo json_encode($result );
+        die;
+    } 
+
+    if(empty($email)){
+
+        $result = array( 'response' => 'error', 'empty'=>'email', 'message'=>'<strong>Error!</strong> El correo está vacío.' );
+        echo json_encode($result );
+        die;
+    } 
+
+    if(empty($message)){
+
+         $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Error!</strong> El mensaje está vacío.' );
+         echo json_encode($result );
+         die;
+    }
+    
+
+
+    $headers  = "From: " . $name . ' <' . $email . '>' . "\r\n";
+    $headers .= "Reply-To: ". $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+
+    $templateTags =  array(
+        '{{subject}}' => $subject,
+        '{{email}}'   =>$email,
+        '{{message}}' =>$message,
+        '{{name}}'    =>$name
+        );
+
+
+    $templateContents = file_get_contents( dirname(__FILE__) . '/email-templates/'.$email_template);
+
+    $contents =  strtr($templateContents, $templateTags);
+
+    if ( mail( $to, $subject, $contents, $headers ) ) {
+        $result = array( 'response' => 'success', 'message'=>'<strong>Éxito!</strong> Correo enviado correctamente.' );
+    } else {
+        $result = array( 'response' => 'error', 'message'=>'<strong>Error!</strong> No se puede enviar el correo.'  );
+    }
+
+    echo json_encode( $result );
+
+    die;
